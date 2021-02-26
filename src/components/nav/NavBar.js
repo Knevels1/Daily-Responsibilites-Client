0a38216af240fba8ff6375db55dd70c49dd65815
Knevels1 <kyle.a.nevels@icloud.com>
2021-02-26 09:29:21 -0600

    set local storage to set the right token then updated the nav bar to the correct information and fixed login so that now a user can login and register

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -6,24 +6,24 @@ export const NavBar = (props) => {
     return (
         <ul className="navbar">
             <li className="navbar__item">
-            <Link className="nav-link" to="/">Games</Link>
+            <Link className="nav-link" to="/">All Tasks</Link>
             </li>
             <li className="navbar__item">
-            <Link className="nav-link" to="/events">Events</Link>
+            <Link className="nav-link" to="/completed">Completed Tasks</Link>
             </li>
             <li className="navbar__item">
-            <Link className="nav-link" to="/profile">Profile</Link>
+            <Link className="nav-link" to="/create">Add Task</Link>
             </li>
             {
-                (localStorage.getItem("lu_token") !== null) ?
-                    <li className="nav-item">
+                (localStorage.getItem("Task_user") !== null) ?
+                    <div className="nav-item">
                         <button className="nav-link fakeLink"
                             onClick={() => {
-                                localStorage.removeItem("lu_token")
+                                localStorage.removeItem("Task_user")
                                 props.history.push({ pathname: "/" })
                             }}
                         >Logout</button>
-                    </li> :
+                    </div> :
                     <>
                         <li className="nav-item">
                             <Link className="nav-link" to="/login">Login</Link>
